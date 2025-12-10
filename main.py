e901c2c5ad14005cbe7cad8fba9ea303951842fd
dsb4k8 <dsb4k8@gamil.com>
2025-12-10 01:04:06 -0600

    swap logic, will clean up

--- main.py
+++ main.py
@@ -22,7 +22,35 @@ def initialize(n_nodes: int) -> dict[int, list[str]]:
         ordered_preferences[i] = data
     return ordered_preferences
 
+def swap(idx1: int, idx2: int, d: dict[int, list[str]]) -> dict[int, list[str]]:
+    res = d
+    keys = list(d.keys())
+
+    first = keys[idx1]
+    second = keys[idx2]
+
+    res[first], res[second] = res[second], res[first]
+    return res
+
 if __name__ == '__main__':
     states = initialize(3)
-    for pair in zip(states.keys(), states.values()):
+
+    idx1 = random.randint(0, len(states) - 1)
+    idx2 = random.randint(0, len(states) - 1)
+
+    results = zip(states.keys(), states.values())
+    for pair in results:
         print(pair)
+
+    swapped = swap(idx1, idx2, states)
+    # k, v = swapped.keys(), swapped.values()
+    swapped_results = zip(swapped.keys(), swapped.values())
+
+    print(f'\nSwapping idx1: {idx1}, idx2: {idx2}')
+    for pair in swapped_results:
+        print(pair)
+
+    print()
+
+
+
